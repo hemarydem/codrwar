b714cbf237b0170d2f5d64970f1828837f2c1d80
R H <m.rehamed@gmail.com>
2020-11-12 16:31:26 +0100

    première tentative

---
+++ countCherInString/index.js
@@ -0,0 +1,27 @@
+
+
+let array  = [];
+array [0] = 'abcdeee';
+array [1] = '';
+array [2] = 'aujourd\'huis ça va mieux';
+let i = 0;
+array.forEach(a =>{
+    console.log(count(array));
+});
+
+function count (string) {  
+    if(string == '') return {};
+    let i;
+    let letter;
+    let majArray = [[]];
+    i = 65;
+    while(i<26){
+
+    }
+    let asciiVal = '';
+    string.forEach(s =>{
+      letter = string.charCodeAt(i);
+        i++;
+    });
+    string.charCodeAt
+}
